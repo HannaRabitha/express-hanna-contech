fab23f40a0b6ad5c01354cd08e593be6989d4138
Hanna Rabitha Hasni <h.rabitha@gmail.com>
2022-06-17 14:41:04 +0700

    counter

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require("express");
 var router = express.Router();
 
+var counter = 0;
+
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Hi! its me Hanna" });
+  res.render("index", { title: "Hi! its me Hanna", counter: counter });
+});
+
+/* POST inc counter ++. */
+router.post("/", function (req, res, next) {
+  counter += 1;
+  res.redirect("/");
 });
 
 module.exports = router;
